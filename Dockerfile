ef6fd6c65c2eba5f635b58a0810a4397f5067362
AlfreReinoso <alfrereinoso96@gmail.com>
2023-09-15 01:34:30 -0300

    volvemos para atras

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,4 @@ FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
 COPY target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
-ENV JVM_HEAP_SIZE=1g
-ENV HOST '0.0.0.0'
 EXPOSE 8080
--- src/main/java/eventos/eventos/EventosApplication.java
+++ src/main/java/eventos/eventos/EventosApplication.java
@@ -10,10 +10,7 @@ public class EventosApplication {
 
 	public static void main(String[] args) {
 
-		// SpringApplication.run(EventosApplication.class, args);
-		SpringApplication app = new SpringApplication(EventosApplication.class);
-		String port = System.getenv("PORT");
-		app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
-		app.run(args);
+		SpringApplication.run(EventosApplication.class, args);
+
 	}
 }
