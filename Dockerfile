fea0a7759ccbdcb624267e093d0b67c8b8c8730e
AlfreReinoso <alfrereinoso96@gmail.com>
2023-09-15 01:07:04 -0300

    docker configuracion

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
 COPY target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
+ENV JVM_HEAP_SIZE=1g
 EXPOSE 8080
--- src/main/java/eventos/eventos/EventosApplication.java
+++ src/main/java/eventos/eventos/EventosApplication.java
@@ -3,10 +3,17 @@ package eventos.eventos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class EventosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventosApplication.class, args);
+
+		// SpringApplication.run(EventosApplication.class, args);
+		SpringApplication app = new SpringApplication(EventosApplication.class);
+		String port = System.getenv("PORT");
+		app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
+		app.run(args);
 	}
 }
